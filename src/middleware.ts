9651cdc95edbda31e26bc9db8f9ca5958e0e7e94
SAMKIEL <samuelezekiel488@gmail.com>
2026-02-13 02:37:57 +0100

    is up to date with 'main/main'.

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - api/admin/upload-file (large file uploads)
+     * - api/admin/delete-file (file management)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api/admin/upload-file|api/admin/delete-file|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
