f3bf4e436e746bf01585bf8aea01fa7dd23840a9
SAMKIEL <samuelezekiel488@gmail.com>
2026-02-16 09:16:36 +0100

    fix: Correct Cloudinary image hostname and separate remote patterns configuration.

--- next.config.ts
+++ next.config.ts
@@ -11,8 +11,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "*.supabase.co",
+      },
+      {
         protocol: "https",
-        hostname: "*res.cloudinary.com",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
