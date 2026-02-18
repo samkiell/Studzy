5aa3ced6a7877f5d91ed725ade96db376cbc65e9
SAMKIEL <samuelezekiel488@gmail.com>
2026-02-18 04:27:55 +0100

    refactor: remove PWA integration from Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,3 @@
-import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
-
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
@@ -29,4 +19,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
