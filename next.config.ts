ae7fb4386faf70d71294b9d5a70aba846500dbd6
SAMKIEL <samuelezekiel488@gmail.com>
2026-02-18 04:34:10 +0100

    feat: configure next-pwa to enable Progressive Web App features

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+});
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -21,4 +28,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
