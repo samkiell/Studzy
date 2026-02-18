9aa6bb0286502d8935eaba0a13aad47556123d60
SAMKIEL <samuelezekiel488@gmail.com>
2026-02-18 01:40:20 +0100

    feat: Add utility functions for combining class names, retrieving the base URL, and shuffling arrays.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,3 +17,12 @@ export function getURL() {
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
   return url;
 }
+
+export function shuffle<T>(array: T[]): T[] {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+}
