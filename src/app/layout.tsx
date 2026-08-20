2907c1bf66bc262f51379cc0d3411ff56a98be65
SAMKIEL <samuelezekiel488@gmail.com>
2026-08-20 06:14:56 +0100

    feat(theme): set dark mode as default first choice unless changed to light in settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -93,10 +93,10 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               try {
-                if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                  document.documentElement.classList.add('dark');
-                } else {
+                if (localStorage.theme === 'light') {
                   document.documentElement.classList.remove('dark');
+                } else {
+                  document.documentElement.classList.add('dark');
                 }
               } catch (_) {}
             `
